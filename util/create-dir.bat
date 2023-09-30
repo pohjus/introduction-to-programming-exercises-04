865cb43f65d807f8b3ce190e0064c708d34060f7
pohjus <jussi.pohjolainen@gmail.com>
2023-09-30 11:52:37 +0300

    :hammer: Update create scripts
    Now uses javadoc and expects user name given.

--- util/create-dir.bat
+++ util/create-dir.bat
@@ -2,11 +2,12 @@
 setlocal enabledelayedexpansion
 
 if "%1"=="" (
-  echo Error: No argument provided. Usage: create.bat N
+  echo Error: No argument provided. Usage: create.bat N AuthorName
   exit /b 1
 )
 
 set N=%1
+set AUTHOR=%2
 
 for /l %%i in (1, 1, %N%) do (
   set "pad=00%%i"
@@ -18,7 +19,17 @@ for /l %%i in (1, 1, %N%) do (
     goto end
   )
 
-  echo public class Main > !folder!/Main.java
+  echo /** > !folder!/Main.java
+  echo  * Main class for exercise. >> !folder!/Main.java
+  echo  *  >> !folder!/Main.java
+  echo  * @author %AUTHOR% >> !folder!/Main.java
+  echo  */ >> !folder!/Main.java
+  echo public class Main >> !folder!/Main.java
+  echo     /** >> !folder!/Main.java
+  echo      * Main method - starting point for the app. >> !folder!/Main.java
+  echo      * >> !folder!/Main.java
+  echo      * @param args Command line arguments >> !folder!/Main.java
+  echo      */ >> !folder!/Main.java
   echo     public static void main(String[] args) >> !folder!/Main.java
   echo. >> !folder!/Main.java
   echo     } >> !folder!/Main.java
--- util/create-dir.sh
+++ util/create-dir.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-# in unix systems, give permissions:
 # chmod +x create-dir.sh
 
 if [ -z "$1" ]; then
-  echo "Error: No argument provided. Usage: ./create.sh N"
+  echo "Error: No argument provided. Usage: ./create.sh N AuthorName"
   exit 1
 fi
 
 N=$1
+AUTHOR=$2
 
 for i in $(seq 1 $N); do
   padded=$(printf "%02d" $i)
   dir="e$padded"
   mkdir "$dir"
   cat <<EOL > "$dir/Main.java"
+/**
+ * Main class for exercise.
+ *
+ * @author $AUTHOR
+ */
 public class Main {
+    /**
+     * Main method - starting point for the app.
+     *
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
 
     }
